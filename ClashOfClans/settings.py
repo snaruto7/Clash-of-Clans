a1f77c82857918056b551500712bc618eedf1cbd
Parmeet Singh <parmeets932@IN.nam.ad.pwcinternal.com>
2020-08-21 23:43:24 +0530

    credentials removed

--- ClashOfClans/settings.py
+++ ClashOfClans/settings.py
@@ -79,15 +79,6 @@ DATABASES = {
     'default': {
         #'ENGINE': 'django.db.backends.sqlite3',
         #'NAME': BASE_DIR / 'db.sqlite3',
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
-        'USER': 'cocadmin@acc-developer-2020',
-        'PASSWORD': 'Qwerty123@',
-        'HOST': 'acc-developer-2020.postgres.database.azure.com',
-        'PORT': '5432',
-        'OPTIONS': {
-            'sslmode': 'require',
-        }
     }
 }
 
